ad071e77d1b00bf67cfd2967c52e69f8ffb72b1f
Maitray Bhardwaj <maitraybhardwaj@gmail.com>
2022-06-08 19:39:37 +0530

    Completed Blurry-Loading

---
+++ Blurry-Loading/index.js
@@ -0,0 +1,22 @@
+const body = document.body
+const main = document.querySelector('#main')
+
+let blur = 25, percent = 0, alpha = 1
+
+const reduceBlur = () => {
+	blur = blur - 0.25
+	percent = percent + 1
+	alpha = alpha - 0.01
+
+	body.style.backdropFilter = `blur(${blur}px)`
+	main.style.color = `rgba(255,255,255,${alpha})`
+	main.innerHTML = `${percent}%`
+	if(percent == 100){
+		main.innerHTML = ''
+	}
+}
+
+window.addEventListener('load', () => {
+	const intervalID = setInterval(reduceBlur, 40)
+	setTimeout(() => clearInterval(intervalID), 4000)
+})
